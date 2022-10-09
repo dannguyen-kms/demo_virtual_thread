8a33a02d39d4948336f4efceb7a60f05f13e116c
dannguyen <hieudankt@gmail.com>
2022-10-09 20:27:24 +0700

    first commit

--- src/main/java/org/example/Compare.java
+++ src/main/java/org/example/Compare.java
@@ -4,44 +4,50 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class Compare {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Compare compare = new Compare();
 //        compare.runOldThread();
         compare.runVirtualThread();
     }
-    final AtomicInteger atomicInteger = new AtomicInteger();
-    Runnable runnable = () -> {
-        try {
-            Thread.sleep(Duration.ofSeconds(10));
-        } catch(Exception e) {
-            System.out.println(e);
+
+    public void runOldThread() throws InterruptedException {
+        var threads = IntStream.range(0,100_000).mapToObj(
+                i -> new Thread(()->{
+                    try {
+                        Thread.sleep(5_000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+        ).toList();
+        var i =0;
+        for(var thread:threads){
+            System.out.println(i++);
+            thread.start();
         }
-        System.out.println("Work Done - " + atomicInteger.incrementAndGet());
-    };
-    Instant start = Instant.now();
-    public void runOldThread(){
-        try (var executor = Executors.newFixedThreadPool(20_000)) {
-            for(int i = 0; i < 20_000; i++) {
-                executor.submit(runnable);
-            }
+        for(var thread:threads){
+            thread.join();
         }
-        Instant finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
-        System.out.println("Total elapsed time : " + timeElapsed);
     }
 
     public void runVirtualThread(){
-        Instant start = Instant.now();
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            for(int i = 0; i < 20_000; i++) {
-                executor.submit(runnable);
-            }
+        var threads = IntStream.range(0,100_000).mapToObj(
+                i -> Thread.ofVirtual().unstarted(()->{
+                    try {
+                        Thread.sleep(5_000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+        ).toList();
+        var i =0;
+        for(var thread:threads){
+            System.out.println(i++);
+            thread.start();
         }
-        Instant finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
-        System.out.println("Total elapsed time : " + timeElapsed);
     }
 
 }
